03ea9a19d4b05c97b345947529d1e961e09f0d46
Teddy <brianrcdteodoro@gmail.com>
2020-02-17 21:24:15 +0800

    Created HourlyForecast Component

--- app/components/HourlyForecast/index.js
+++ app/components/HourlyForecast/index.js
@@ -5,20 +5,64 @@
  */
 
 import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
 // import styled from 'styled-components';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { FormattedDate } from 'react-intl';
+import {
+  makeSelectWeatherForecast,
+  makeSelectCity,
+} from '../../containers/WeatherForecast/selectors';
 
-function HourlyForecast() {
+function HourlyForecast(props) {
+  const dayData = props.weatherForecastPage.hourlyData[
+    props.weatherForecastPage.index
+  ].map(data => {
+    const celsiusMin = data.main.temp_min - 273.15;
+    const celsiusMax = data.main.temp_max - 273.15;
+    return (
+      <div key={data.dt}>
+        <FormattedDate
+          value={new Date(data.dt_txt)}
+          weekday="short"
+          hour="numeric"
+          timeZoneName="short"
+        />
+        <span>Highest temp: {celsiusMax.toFixed(2)}&deg; </span>
+        <span>Lowest temp: {celsiusMin.toFixed(2)}&deg;</span>
+        <img
+          src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+          alt={data.weather[0].description}
+        />
+        {data.weather[0].main}
+      </div>
+    );
+  });
   return (
     <div>
-      <FormattedMessage {...messages.header} />
+      <h1>{props.city}</h1>
+      {dayData}
     </div>
   );
 }
 
-HourlyForecast.propTypes = {};
+HourlyForecast.propTypes = {
+  weatherForecastPage: PropTypes.object.isRequired,
+  city: PropTypes.string,
+};
 
-export default memo(HourlyForecast);
+const mapStateToProps = createStructuredSelector({
+  weatherForecastPage: makeSelectWeatherForecast(),
+  city: makeSelectCity(),
+});
+
+const withConnect = connect(mapStateToProps);
+
+export default compose(
+  withConnect,
+  memo,
+)(HourlyForecast);
