3f416770bc3373840f7b2fdf314e90eabcb9d095
Teddy <brianrcdteodoro@gmail.com>
2020-02-17 20:49:30 +0800

    fixed WeatherForecast container

--- app/containers/WeatherForecast/index.js
+++ app/containers/WeatherForecast/index.js
@@ -14,12 +14,17 @@ import { compose } from 'redux';
 
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
-import makeSelectWeatherForecast from './selectors';
+import makeSelectWeatherForecast, { makeSelectCity } from './selectors';
+import { filterDaily } from './actions';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
 
-export function WeatherForecast() {
+import Search from '../../components/Search';
+import Loading from '../../components/Loading';
+import DailyForecast from '../../components/DailyForecast';
+
+export function WeatherForecast(props) {
   useInjectReducer({ key: 'weatherForecast', reducer });
   useInjectSaga({ key: 'weatherForecast', saga });
 
@@ -29,24 +34,36 @@ export function WeatherForecast() {
         <title>WeatherForecast</title>
         <meta name="description" content="Description of WeatherForecast" />
       </Helmet>
+      <h1>5 Day Weather Forecast</h1>
+      {props.weatherForecastPage.isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <Search />
+          <br />
+          <br />
+          {props.weatherForecastPage.dailyData.map((day, index) => (
+            <DailyForecast key={day.dt} index={index} day={day} />
+          ))}
+        </div>
+      )}
       <FormattedMessage {...messages.header} />
     </div>
   );
 }
 
 WeatherForecast.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  weatherForecastPage: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
   weatherForecast: makeSelectWeatherForecast(),
+  city: makeSelectCity(),
 });
 
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
+const mapDispatchToProps = dispatch => ({
+  filterDaily: data => dispatch(filterDaily(data)),
+});
 
 const withConnect = connect(
   mapStateToProps,
